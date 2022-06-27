47014bd5139cfce1a4e38401334bc07d2d912e2c
Ghostreverie91 <maxmkt91@gmail.com>
2022-06-27 13:00:45 -0400

    tracklist

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,38 +2,39 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Album = (props) => {
-    const { basic_information } = props;
-    const [tracklist, setTracklist] = useState({});
-    // useEffect(() => {
-    //     axios
-    //         .get(basic_information.ressource_url)
-    //         .then((res) => setTracklist(res.data.tracklist));
-    // }, [basic_information]);
-    if (!basic_information) {
-        return null;
-    }
+  const { basic_information } = props;
+  console.log(props.basic_information.resource_url);
+  const [tracklist, setTracklist] = useState({});
 
-    return (
-        <div className="album">
-            <img src={basic_information.cover_image} alt="pochette"></img>
-            <div>
-                <h1>{basic_information.artists[0].name}</h1>
-                <h2>{basic_information.title}</h2>
-                {basic_information.styles.map((style) => (
-                    <h4>{style}</h4>
-                ))}
-            </div>
-            {/* <div>
-                <ol>
-                    {tracklist.map((track) => (
-                        <li>
-                            {track.title} -- {track.duration}
-                        </li>
-                    ))}
-                </ol>
-            </div> */}
-        </div>
-    );
+  useEffect(() => {
+    axios
+      .get(basic_information.resource_url)
+      .then((res) => setTracklist(res.data.tracklist));
+  }, [basic_information]);
+  if (!basic_information) {
+    return null;
+  }
+
+  return (
+    <div className="album">
+      <img src={basic_information.cover_image} alt="pochette"></img>
+      <div>
+        <h1>{basic_information.artists[0].name}</h1>
+        <h2>{basic_information.title}</h2>
+
+        {basic_information.styles.map((style) => (
+          <h4>{style}</h4>
+        ))}
+      </div>
+      <div>
+        <ol>
+          {tracklist.map((track) => (
+            <li>{track.title}</li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
 };
 
 export default Album;
