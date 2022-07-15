d562529a98bece1a948a6dac46283be65fdde148
Maxime McKay-Turgeon <maxmkt91@gmail.com>
2022-07-15 07:41:13 -0400

    spotify create playlist + push tracks

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,165 +3,307 @@ import React, { useEffect, useState } from "react";
 import { Col, Row, Image } from "react-bootstrap";
 
 const Album = (props) => {
-	const { basic_information, hideAddButton, tokenSpotify, id } = props;
-	const [confirmation, setConfirmation] = useState("");
-	const [playlistIsLoading, setPlaylistIsLoading] = useState(false);
-	const [disableButton, setDisableButton] = useState(false);
-	const [searchResultSpotify, setSearchResultSpotify] = useState([]);
-	const [exactArtistID, setExactArtistID] = useState("");
+    const { basic_information, hideAddButton, tokenSpotify, id, spotifyID } =
+        props;
+    const [confirmation, setConfirmation] = useState("");
+    const [playlistIsLoading, setPlaylistIsLoading] = useState(false);
+    const [disableButton, setDisableButton] = useState(false);
+    //spotify
+    const [searchResultSpotify, setSearchResultSpotify] = useState([]);
+    const [exactArtistID, setExactArtistID] = useState("");
+    const [artistAlbumsSpotify, setArtistAlbumsSpotify] = useState([]);
+    const [albumSpotifyId, setAlbumSpotifyId] = useState("");
+    const [tracks, setTracks] = useState([]);
+    const [tracksIDs, setTrackIDs] = useState([]);
+    const [playlistSpotifyExists, setPlaylistSpotifyExists] = useState(false);
+    const [playlistSpotifyID, setPlaylistSpotifyID] = useState("");
+    const [addTracks, setAddTracks] = useState(false);
 
-	useEffect(() => {
-		if (tokenSpotify) {
-			console.log(`name: ${basic_information.name}`);
-			axios
-				.get("https://api.spotify.com/v1/search", {
-					headers: {
-						Authorization: `Bearer ${tokenSpotify}`,
-					},
-					params: {
-						q: basic_information.artists[0].name,
-						type: "artist",
-					},
-				})
-				.then((res) => {
-					setSearchResultSpotify(res.data.artists.items);
-				});
-		}
-	}, []);
+    let tracksArr = [];
+    console.log(spotifyID);
 
-	useEffect(() => {
-		for (let artist of searchResultSpotify) {
-			if (artist.name === basic_information.artists[0].name) {
-				setExactArtistID(artist.id);
-				console.log(`id artiste: ${artist.id}`);
-			}
-		}
-		console.log(exactArtistID);
-	}, [searchResultSpotify]);
+    //recherche spotify pour le nom d'artiste de l'api discogs
+    useEffect(() => {
+        if (tokenSpotify) {
+            axios
+                .get("https://api.spotify.com/v1/search", {
+                    headers: {
+                        Authorization: `Bearer ${tokenSpotify}`
+                    },
+                    params: {
+                        q: basic_information.artists[0].name,
+                        type: "artist"
+                    }
+                })
+                .then((res) => {
+                    setSearchResultSpotify(res.data.artists.items);
+                });
+        }
+    }, []);
 
-	if (!basic_information) {
-		return null;
-	}
+    useEffect(() => {
+        if (tokenSpotify) {
+            axios
+                .get(`	https://api.spotify.com/v1/me/playlists`, {
+                    headers: {
+                        Authorization: `Bearer ${tokenSpotify}`
+                    }
+                })
+                .then((res) => {
+                    let playlists = res.data.items;
+                    for (let playlist of playlists) {
+                        if (playlist.name == "Spinset") {
+                            setPlaylistSpotifyID(playlist.id);
+							
+                            console.log(`get playlist id ${playlist.id}`);
+                            break;
+                        }
+                    }
+                });
+        }
+    }, []);
 
-	console.log(searchResultSpotify);
+    //Cherche un === nom d'artiste spotify et extrait son ID
+    useEffect(() => {
+        for (let artist of searchResultSpotify) {
+            if (artist.name === basic_information.artists[0].name) {
+                setExactArtistID(artist.id);
+                break;
+            }
+        }
+    }, [searchResultSpotify]);
 
-	const addToPlaylist = (e) => {
-		setPlaylistIsLoading(true);
-		axios
-			.post("https://spinset-db.herokuapp.com/albums", basic_information)
-			.then(() => {
-				setConfirmation("C'est ajouté!");
-				setPlaylistIsLoading(false);
-				setDisableButton(true);
-			});
-	};
+    //Va chercher la discography de l'ID
+    useEffect(() => {
+        console.log(`artist id: ${exactArtistID}`);
+        axios
+            .get(`https://api.spotify.com/v1/artists/${exactArtistID}/albums`, {
+                headers: {
+                    Authorization: `Bearer ${tokenSpotify}`
+                }
+            })
+            .then((res) => {
+                setArtistAlbumsSpotify(res.data.items);
+            });
+    }, [exactArtistID]);
 
-	const deleteFromPlaylist = () => {
-		setPlaylistIsLoading(true);
-		axios.delete(`https://spinset-db.herokuapp.com/albums/${id}`).then(() => {
-			setConfirmation("C'est deleté!");
-			setPlaylistIsLoading(false);
-			setDisableButton(true);
-		});
-	};
-	console.log(`id: ${id}`);
+    useEffect(() => {
+        for (let album of artistAlbumsSpotify) {
+            if (album.name == basic_information.title) {
+                setAlbumSpotifyId(album.id);
+                console.log(`album id: ${albumSpotifyId}`);
+                break;
+            }
+        }
+    }, [artistAlbumsSpotify]);
 
-	return (
-		<div className="album">
-			<Row className="justify-content-center my-3 p-3 shadow-lg rounded">
-				<Col lg={6}>
-					<Image
-						fluid
-						src={basic_information.cover_image}
-						alt="pochette"
-					></Image>
-				</Col>
-				<Col lg={6} className="">
-					<h1 className="mt-3 mt-lg-0">{basic_information.artists[0].name}</h1>
-					<h2>{basic_information.title}</h2>
-					<h3>{basic_information.year}</h3>
-					<div>
-						{basic_information &&
-							basic_information.styles.map((style) => (
-								<span className="style">{style}</span>
-							))}
-					</div>
-					{!hideAddButton && (
-						<button
-							type="button"
-							className="fancy mt-3"
-							onClick={addToPlaylist}
-							disabled={disableButton}
-						>
-							<span className="top-key"></span>
-							<span className="text">
-								{playlistIsLoading ? (
-									<div className="loading">
-										<span></span>
-										<span></span>
-										<span></span>
-									</div>
-								) : (
-									"Ajouter à la Playlist"
-								)}
-							</span>
-							<span className="bottom-key-1"></span>
-							<span className="bottom-key-2"></span>
-						</button>
-					)}
-					{hideAddButton && (
-						<button
-							type="button"
-							className="fancy mt-3"
-							onClick={deleteFromPlaylist}
-							disabled={disableButton}
-						>
-							<span className="top-key"></span>
-							<span className="text">
-								{" "}
-								{playlistIsLoading ? (
-									<div className="loading">
-										<span></span>
-										<span></span>
-										<span></span>
-									</div>
-								) : (
-									"Deleter de la Playlist"
-								)}
-							</span>
-							<span className="bottom-key-1"></span>
-							<span className="bottom-key-2"></span>
-						</button>
-					)}
-					{tokenSpotify && (
-						<button
-							type="button"
-							className="fancy mt-3"
-							// onClick={deleteFromPlaylist}
-							// disabled={disableButton}
-						>
-							<span className="top-key"></span>
-							<span className="text">
-								{" "}
-								{playlistIsLoading ? (
-									<div className="loading">
-										<span></span>
-										<span></span>
-										<span></span>
-									</div>
-								) : (
-									"Ajouter à ton Spotify"
-								)}
-							</span>
-							<span className="bottom-key-1"></span>
-							<span className="bottom-key-2"></span>
-						</button>
-					)}
-					<div>{confirmation}</div>
-				</Col>
-			</Row>
-		</div>
-	);
+    useEffect(() => {
+        console.log(albumSpotifyId);
+        axios
+            .get(`	https://api.spotify.com/v1/albums/${albumSpotifyId}/tracks`, {
+                headers: {
+                    Authorization: `Bearer ${tokenSpotify}`
+                }
+            })
+            .then((res) => {
+                setTracks(res.data.items);
+            });
+    }, [albumSpotifyId]);
+
+    useEffect(() => {
+        setTrackIDs(tracks.map((track) => `spotify:track:${track.id}`));
+    }, [tracks]);
+
+    // useEffect(() => {
+    // 	createSpotifyPlaylist();
+    // }, [addTracks]);
+
+    const createSpotifyPlaylist = () => {
+        if (!playlistSpotifyExists) {
+            axios
+                .post(
+                    `https://api.spotify.com/v1/users/${spotifyID}/playlists`,
+                    {
+                        name: "Spinset",
+                        public: true,
+                        description: "Tsé là ce qu'on écoutait chez max..."
+                    },
+
+                    {
+                        headers: {
+                            Authorization: `Bearer ${tokenSpotify}`
+                        }
+                    }
+                )
+                .then(() => {
+                    setPlaylistSpotifyExists(true);
+                });
+        } else {
+            setPlaylistSpotifyExists(true);
+        }
+    };
+
+    const addAlbumTracks = () => {
+        console.log(`uris: ${tracksIDs}`);
+        axios.post(
+            `https://api.spotify.com/v1/playlists/${playlistSpotifyID}/tracks`,
+            { uris: tracksIDs },
+            {
+                headers: {
+                    Authorization: `Bearer ${tokenSpotify}`
+                }
+            }
+        );
+    };
+
+    const addToPlaylist = (e) => {
+        setPlaylistIsLoading(true);
+        axios
+            .post("https://spinset-db.herokuapp.com/albums", basic_information)
+            .then(() => {
+                setConfirmation("C'est ajouté!");
+                setPlaylistIsLoading(false);
+                setDisableButton(true);
+            });
+    };
+
+    const deleteFromPlaylist = () => {
+        setPlaylistIsLoading(true);
+        axios
+            .delete(`https://spinset-db.herokuapp.com/albums/${id}`)
+            .then(() => {
+                setConfirmation("C'est deleté!");
+                setPlaylistIsLoading(false);
+                setDisableButton(true);
+            });
+    };
+
+    if (!basic_information) {
+        return null;
+    }
+
+    return (
+        <div className="album">
+            <Row className="justify-content-center my-3 p-3 shadow-lg rounded">
+                <Col lg={6}>
+                    <Image
+                        fluid
+                        src={basic_information.cover_image}
+                        alt="pochette"
+                    ></Image>
+                </Col>
+                <Col lg={6} className="">
+                    <h1 className="mt-3 mt-lg-0">
+                        {basic_information.artists[0].name}
+                    </h1>
+                    <h2>{basic_information.title}</h2>
+                    <h3>{basic_information.year}</h3>
+                    <div>
+                        {basic_information &&
+                            basic_information.styles.map((style) => (
+                                <span className="style">{style}</span>
+                            ))}
+                    </div>
+                    {!hideAddButton && (
+                        <button
+                            type="button"
+                            className="fancy mt-3"
+                            onClick={addToPlaylist}
+                            disabled={disableButton}
+                        >
+                            <span className="top-key"></span>
+                            <span className="text">
+                                {playlistIsLoading ? (
+                                    <div className="loading">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                ) : (
+                                    "Ajouter à la Playlist"
+                                )}
+                            </span>
+                            <span className="bottom-key-1"></span>
+                            <span className="bottom-key-2"></span>
+                        </button>
+                    )}
+                    {hideAddButton && (
+                        <button
+                            type="button"
+                            className="fancy mt-3"
+                            onClick={deleteFromPlaylist}
+                            disabled={disableButton}
+                        >
+                            <span className="top-key"></span>
+                            <span className="text">
+                                {" "}
+                                {playlistIsLoading ? (
+                                    <div className="loading">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                ) : (
+                                    "Deleter de la Playlist"
+                                )}
+                            </span>
+                            <span className="bottom-key-1"></span>
+                            <span className="bottom-key-2"></span>
+                        </button>
+                    )}
+                    {tokenSpotify && (
+                        <>
+                            <button
+                                type="button"
+                                className="fancy mt-3"
+                                onClick={addAlbumTracks}
+                                // disabled={disableButton}
+                            >
+                                <span className="top-key"></span>
+                                <span className="text">
+                                    {" "}
+                                    {playlistIsLoading ? (
+                                        <div className="loading">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    ) : (
+                                        "Ajouter à ton Spotify"
+                                    )}
+                                </span>
+                                <span className="bottom-key-1"></span>
+                                <span className="bottom-key-2"></span>
+                            </button>
+                            <button
+                                type="button"
+                                className="fancy mt-3"
+                                onClick={createSpotifyPlaylist}
+                                // disabled={disableButton}
+                            >
+                                <span className="top-key"></span>
+                                <span className="text">
+                                    {" "}
+                                    {playlistIsLoading ? (
+                                        <div className="loading">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+                                    ) : (
+                                        "Créer une playlist spotify \"spinset\""
+                                    )}
+                                </span>
+                                <span className="bottom-key-1"></span>
+                                <span className="bottom-key-2"></span>
+                            </button>
+                        </>
+                    )}
+                    <div>{confirmation}</div>
+                </Col>
+            </Row>
+        </div>
+    );
 };
 
 export default Album;
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,99 +4,113 @@ import Album from "./Album";
 import { Col, Row } from "react-bootstrap";
 
 const Playlist = () => {
-	const CLIENT_ID = "95383cdfac9943cf814a92aefe535ab5";
-	const REDIRECT_URI = "http://localhost:8888/";
-	const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-	const RESPONSE_TYPE = "token";
-	const [playlist, setPlaylist] = useState([]);
-	const [token, setToken] = useState("");
+    const CLIENT_ID = "95383cdfac9943cf814a92aefe535ab5";
+    const REDIRECT_URI = "http://localhost:8888/";
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+    const RESPONSE_TYPE = "token";
+    const SCOPE =
+        "playlist-modify-private playlist-modify-public user-library-modify playlist-modify-public playlist-read-private";
+    const [playlist, setPlaylist] = useState([]);
+    const [token, setToken] = useState("");
+    const [spotifyID, setSpotifyID] = useState("");
 
-	useEffect(() => {
-		axios
-			.get("https://spinset-db.herokuapp.com/albums")
-			.then((res) => setPlaylist(res.data));
-	}, []);
+    useEffect(() => {
+        axios
+            .get("https://spinset-db.herokuapp.com/albums")
+            .then((res) => setPlaylist(res.data));
+    }, []);
 
-	console.log(playlist);
+    console.log(playlist);
 
-	//check l'url pour le token spotify et l'extrait dans token
-	useEffect(() => {
-		const hash = window.location.hash;
-		let token = window.localStorage.getItem("token");
+    //check l'url pour le token spotify et l'extrait dans token
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem("token");
 
-		if (!token && hash) {
-			token = hash
-				.substring(1)
-				.split("&")
-				.find((elem) => elem.startsWith("access_token"))
-				.split("=")[1];
+        if (!token && hash) {
+            token = hash
+                .substring(1)
+                .split("&")
+                .find((elem) => elem.startsWith("access_token"))
+                .split("=")[1];
 
-			window.location.hash = "";
-			window.localStorage.setItem("token", token);
-		}
+            window.location.hash = "";
+            window.localStorage.setItem("token", token);
+        }
 
-		setToken(token);
-	}, []);
+        setToken(token);
+    }, []);
 
-	const logout = () => {
-		setToken("");
-		window.localStorage.removeItem("token");
-	};
+    useEffect(() => {
+        axios
+            .get(`	https://api.spotify.com/v1/me`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then((res) => setSpotifyID(res.data.id));
+    }, [token]);
 
-	return (
-		<div>
-			{!token ? (
-				<Row className="text-center mt-3">
-					<Col lg={6}>
-						<h2>
-							Log toi sur ton spotify pour pouvoir te faire une playlist avec ce
-							qu'on écoute!
-						</h2>
-					</Col>
-					<Col lg={6}>
-						<a
-							href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-						>
-							<button className="fancy" value="playlist">
-								<span className="top-key"></span>
-								<span className="text">Login Spotify</span>
-								<span className="bottom-key-1"></span>
-								<span className="bottom-key-2"></span>
-							</button>
-						</a>
-					</Col>
-				</Row>
-			) : (
-				<Row className="text-center mt-3">
-					<Col lg={6}>
-						<h2>Bien loggé!</h2>
-					</Col>
-					<Col lg={6}>
-						<button
-							type="button"
-							className="fancy"
-							value="playlist"
-							onClick={logout}
-						>
-							<span className="top-key"></span>
-							<span className="text">Logout</span>
-							<span className="bottom-key-1"></span>
-							<span className="bottom-key-2"></span>
-						</button>
-					</Col>
-				</Row>
-			)}
-			{playlist.map((album) => (
-				<Album
-					id={album.id}
-					basic_information={album}
-					key={album.id}
-					hideAddButton={true}
-					tokenSpotify={token}
-				/>
-			))}
-		</div>
-	);
+    const logout = () => {
+        setToken("");
+        window.localStorage.removeItem("token");
+    };
+
+    return (
+        <div>
+            {!token ? (
+                <Row className="text-center mt-3">
+                    <Col lg={6}>
+                        <h2>
+                            Log toi sur ton spotify pour pouvoir te faire une
+                            playlist avec ce qu'on écoute!
+                        </h2>
+                    </Col>
+                    <Col lg={6}>
+                        <a
+                            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}
+                        >
+                            <button className="fancy" value="playlist">
+                                <span className="top-key"></span>
+                                <span className="text">Login Spotify</span>
+                                <span className="bottom-key-1"></span>
+                                <span className="bottom-key-2"></span>
+                            </button>
+                        </a>
+                    </Col>
+                </Row>
+            ) : (
+                <Row className="text-center mt-3">
+                    <Col lg={6}>
+                        <h2>Bien loggé!</h2>
+                    </Col>
+                    <Col lg={6}>
+                        <button
+                            type="button"
+                            className="fancy"
+                            value="playlist"
+                            onClick={logout}
+                        >
+                            <span className="top-key"></span>
+                            <span className="text">Logout</span>
+                            <span className="bottom-key-1"></span>
+                            <span className="bottom-key-2"></span>
+                        </button>
+                    </Col>
+                </Row>
+            )}
+            {playlist.map((album) => (
+                <Album
+                    id={album.id}
+                    basic_information={album}
+                    key={album.id}
+                    hideAddButton={true}
+                    tokenSpotify={token}
+                    spotifyID={spotifyID}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default Playlist;
